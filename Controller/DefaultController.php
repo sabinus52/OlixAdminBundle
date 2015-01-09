925aefd92ad14da7a89870f703db45749089e5f2
Olivier <sabinus52@gmail.com>
2015-01-09 22:45:59 +0100

    Passage du routing en mode annotation

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -3,11 +3,15 @@
 namespace Olix\AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    /**
+     * @Route("/test-dashboard", name="olix_admin_dashboard")
+    */
+    public function indexAction()
     {
-        return $this->render('OlixAdminBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('OlixAdminBundle:Default:index.html.twig', array('name' => 'World'));
     }
 }
