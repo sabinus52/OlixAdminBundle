1e87dd30c47dfcec8ded203a230241bb4577156e
Olivier <sabinus52@gmail.com>
2015-01-10 11:15:06 +0100

    Création du layout

--- Controller/TestController.php
+++ Controller/TestController.php
@@ -5,13 +5,13 @@ namespace Olix\AdminBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
-class DefaultController extends Controller
+class TestController extends Controller
 {
     /**
      * @Route("/test-dashboard", name="olix_admin_dashboard")
     */
     public function indexAction()
     {
-        return $this->render('OlixAdminBundle:Default:index.html.twig', array('name' => 'World'));
+        return $this->render('OlixAdminBundle:Test:index.html.twig');
     }
 }
