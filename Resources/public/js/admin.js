2966869b7191e3abdf71be896890b4fa7ed513ed
Olivier <sabinus52@gmail.com>
2015-02-23 16:11:43 +0100

    Ajout fonction JS pour la validation d'un formulaire dans un modal

--- Resources/public/js/admin.js
+++ Resources/public/js/admin.js
@@ -250,6 +250,39 @@ var olixAdminInterface = {
         $('#modalDelete form').attr('action', $(obj).attr('href'));
         $('#modalDelete').modal('show');
         return false;
+    },
+
+
+    /**
+     * Soumission d'un formulaire en Ajax depuis une boite de dialogue
+     * 
+     * @param string modal : Nom du modal #modal
+     * @param string form  : Nom du formulaire #form
+     * @param string path  : Chemin du script php
+     */
+    submitFormModalAjax : function(modal, form, path)
+    {
+        $.ajax({
+            type: "POST",
+            url: path,
+            data: $(form).serialize(),
+            cache: false,
+            beforeSend: function() {
+                $(modal+' .modal-body').addClass('text-center').html('<p><img alt="" src="/bundles/olixadmin/images/spinner-rectangle.gif"></p>');
+                $(modal+' .modal-footer').html('');
+            },
+            success: function(data) {
+                if (data == 'OK') {
+                    $(modal).modal('hide');
+                    location.reload();
+                } else {
+                    $(modal+' .modal-dialog').html(data);
+                }
+            },
+            error: function () {
+                alert("Une erreur est survenue lors de validation du formulaire.");
+            }
+        });
     }
 
 };
